3bdd4681aac7a7457f8c59df6b05177e83a0aa2d
Srujan Ds <srujan2182005@gmail.com>
2026-07-09 21:48:11 +0530

    Time: 3 ms (28.26%) | Memory: 12.4 MB (33.02%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        shortest=min(strs,key=len)
+        for i in range(len(shortest)):
+            for word in strs:
+                if word == shortest:
+                    continue
+                if word[i]!=shortest[i]:
+                    return shortest[:i]
+       
+        return shortest
