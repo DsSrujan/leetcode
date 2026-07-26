26bdb6afc4d576350e5a78332c9f3507b3bab80e
Srujan Ds <srujan2182005@gmail.com>
2026-07-26 16:24:14 +0530

    Time: 23 ms (50.48%) | Memory: 14.2 MB (12.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,12 @@
 class Solution(object):
 
   def isAnagram(self, s, t):
-    if len(s) != len(t):
-      return False
-
-    counts = [0] * 26
-
-    for i in range(len(s)):
-      counts[ord(s[i]) - ord('a')] += 1
-      counts[ord(t[i]) - ord('a')] -= 1
-
-    for count in counts:
-      if count != 0:
+    if len(s)!=len(t):
         return False
+    fe1={}
+    fe2={}
+    for i in range(len(s)):
+        fe1[s[i]]=fe1.get(s[i],0)+1
+        fe2[t[i]]=fe2.get(t[i],0)+1
+    return fe1==fe2
 
-    return True
