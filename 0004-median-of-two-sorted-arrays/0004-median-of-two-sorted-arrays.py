d717aaf9037f558affe6a8a1a9648df61bd99d57
Srujan Ds <srujan2182005@gmail.com>
2026-08-13 23:46:17 +0530

    Time: 2 ms (63.78%), Space: 12.5 MB (51.14%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Always binary search on the smaller array
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        m = len(nums1)
+        n = len(nums2)
+
+        left = 0
+        right = m
+
+        while left <= right:
+            cut1 = (left + right) // 2
+            cut2 = (m + n + 1) // 2 - cut1
+
+            # Elements just before and after the cuts
+            left1 = float('-inf') if cut1 == 0 else nums1[cut1 - 1]
+            right1 = float('inf') if cut1 == m else nums1[cut1]
+
+            left2 = float('-inf') if cut2 == 0 else nums2[cut2 - 1]
+            right2 = float('inf') if cut2 == n else nums2[cut2]
+
+            # Correct partition
+            if left1 <= right2 and left2 <= right1:
+
+                # Odd total number of elements
+                if (m + n) % 2 == 1:
+                    return max(left1, left2)
+
+                # Even total number of elements
+                return (max(left1, left2) + min(right1, right2)) / 2.0
+
+            # nums1 cut is too far right
+            elif left1 > right2:
+                right = cut1 - 1
+
+            # nums1 cut is too far left
+            else:
+                left = cut1 + 1
