222f31bfe32f552de5110e8aab9d851dab010b27
Srujan Ds <srujan2182005@gmail.com>
2026-08-25 16:18:17 +0530

    Time: 1 ms (37.93%), Space: 12.3 MB (50.57%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -5,9 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        s=set(nums)
         j=1
         for i in range(len(nums)+1):
-            if k*j  in nums:
+            if k*j  in s:
                 j+=1
             else:
                  return k*j
