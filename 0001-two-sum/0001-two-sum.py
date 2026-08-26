25f0ca67eaaace80dc2ce6ca01cda2b688e2050b
Srujan Ds <srujan2182005@gmail.com>
2026-08-26 12:49:40 +0530

    Time: 3 ms (54.33%), Space: 12.9 MB (95.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums, target):
         dic={}
-        for i, n in enumerate(nums):
-            c=target-n
-            if c in dic:
-                return [dic[c],i]
+        for i,n in enumerate(nums):
+            compliment=target-n
+            if compliment in dic:
+                return [dic[compliment],i]
             dic[n]=i
